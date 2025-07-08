da61f4f4b51fa40552d673c54c88e5e0759821cf
zyaduqu <zyadexec@gmail.com>
2025-07-08 13:10:08 +0300

    add some changes

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -23,12 +23,24 @@ public class Account {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getBalance() {
         return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 
     public int credit(int amount){
--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -15,12 +15,24 @@ public class Employee {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getSalary() {
         return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 
     public int getAnnualSalary() {
